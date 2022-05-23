38ef9e516c2082d2ad2cb753d2708a30f2426e66
Munira Turdiyeva <Umishah2013@gmail.com>
2022-05-23 16:53:37 +0500

    added munira.html and munira.js

---
+++ munira.js
@@ -0,0 +1,15 @@
+"use strict";
+const numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
+console.log(numberOfNews);
+const personalNewsDB = {
+  count: numberOfNews,
+  news: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+const a = prompt("Oxirgi ko'rgan yangiliklarizdan biri?"),
+  b = prompt("Unga qancha baho bergan bo'lar edingiz?");
+
+personalNewsDB.news[a] = b;
+console.log(personalNewsDB);
